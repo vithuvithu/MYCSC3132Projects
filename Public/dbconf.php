d27ddc62873c8f260cf00a0f3ff434bdc1995f1d
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2024-12-01 08:09:39 +0000

    connection code added

---
+++ Public/dbconf.php
@@ -0,0 +1,21 @@
+<?php
+//constant variable
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb'); 
+define('DBNAME', 'school');
+try {
+//connect with database
+$connect = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+if (!$connect) {
+die("connection failed".mysqli_connect_error());
+}
+else {
+//echo "Connected successfully":
+}
+}
+catch (Exception $e) {
+die($e->getMessage());
+}
+//echo "abc<br>";
+?>
