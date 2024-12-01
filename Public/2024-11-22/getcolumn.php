0e21ff3e7a0c8c5a8b7d50123059f8e7353903dd
vithuvithu <138983069+vithuvithu@users.noreply.github.com>
2024-12-01 09:53:33 +0000

    file get data from table

--- Public/2024-11-22/getcolumn.php
+++ Public/2024-11-22/getcolumn.php
@@ -1,1 +1,54 @@
- 
+<?php
+//get the db connection file
+require_once 'dbconf.php';
+
+
+
+function PrintTableCols($tableName,$connect,$colnames)
+{
+	try {
+
+		//Query
+		$sql = "SELECT ";
+		for ($i=0; $i < sizeof($colnames)-1; $i++) { 
+			$sql .=$colnames[$i].",";
+			//echo $sql;
+		}
+		$sql .=$colnames[sizeof($colnames)-1]." from $tableName";
+		//echo $sql;
+		//excute the quey
+
+		$result = mysqli_query($connect,$sql);
+		//check if data exists in the table
+		if (mysqli_num_rows($result)>0) {
+			// fetch the data from rows
+			echo "<table border='1'>";
+			$col = mysqli_fetch_fields($result);
+			//print the colums
+			echo "<tr>";
+			foreach ($col as $value) {
+				//return an object
+				//print_r($value);
+				echo "<td>$value->name</td>";
+			}
+			echo "</tr>";
+			
+			while ($row = mysqli_fetch_assoc($result)) {
+				//print the data in a table format
+				echo "<tr>";
+				foreach ($row as $key => $value) {
+					echo "<td>$value</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "No results";
+		}
+		
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+}
+PrintTableCols("student",$connect,["name","age","gender"]);
+?>
